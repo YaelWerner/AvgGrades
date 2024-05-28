bfbc288f5596ce811fe4116c8edf5975dd6d7859
YaelWerner <yaelbluma@gmail.com>
2024-05-28 15:09:24 +0300

    script.js

--- script.js
+++ script.js
@@ -8,17 +8,11 @@ function addGradeRow() {
     const gradeRow = document.createElement('div');
     gradeRow.classList.add('grade-row');
     gradeRow.innerHTML = `
-        <input type="text" name="courseName" placeholder="שם הקורס">
+        <input type="text" name="courseName" placeholder="שם הקורס" required>
         <input type="number" name="grade" placeholder="ציון" min="0" max="100" required>
         <input type="number" name="weight" placeholder="משקל" min="0" max="100" required>
-        <button type="button" class="remove-course" onclick="removeGradeRow(this)">הסר קורס</button>
     `;
     form.insertBefore(gradeRow, form.children[form.children.length - 2]);
-}
-
-function removeGradeRow(button) {
-    const gradeRow = button.parentElement;
-    gradeRow.remove();
 }
 
 function calculateAverage() {
