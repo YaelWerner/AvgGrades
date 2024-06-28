93310c0c0dcb311f75aa110e57b04ab593ee1a37
YaelWerner <yaelbluma@gmail.com>
2024-06-28 07:08:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,13 @@ function addGradeRow() {
         <input type="number" name="weight" placeholder="משקל" min="0" max="100" required>
         <button type="button" class="remove-course" onclick="removeGradeRow(this)">הסר קורס</button>
     `;
-    const calculateButton = document.querySelector('.calculate-button');
-    document.getElementById('gradesForm').insertBefore(gradeRow, calculateButton);
+    const lastGradeRow = document.querySelector('#gradesForm .grade-row:last-of-type');
+    if (lastGradeRow) {
+        lastGradeRow.after(gradeRow);
+    } else {
+        const calculateButton = document.querySelector('.calculate-button');
+        document.getElementById('gradesForm').insertBefore(gradeRow, calculateButton);
+    }
 }
 
 function removeGradeRow(button) {
